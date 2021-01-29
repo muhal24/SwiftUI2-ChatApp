13f77a4bf68997de26f6f04c8d7d9397001f7230
Muhammed Hanifi Alma <hanifi.alma2014.mha@gmail.com>
2021-01-29 10:36:08 +0300

    Initial Commit

---
+++ SwiftUI2-ChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI2-ChatApp
+//
+//  Created by Muhammed Hanifi Alma on 29.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI2-ChatApp/SwiftUI2_ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2_ChatAppApp.swift
+//  SwiftUI2-ChatApp
+//
+//  Created by Muhammed Hanifi Alma on 29.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2_ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
